b4b9236e9665ac0773766047767144b2ddb00003
KJH <kjhg5852@naver.com>
2021-05-02 01:07:22 +0900

    Hello World

--- lib/main.dart
+++ lib/main.dart
@@ -1,74 +1,22 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    title: 'Navigation Basics',
-    home: FistApp(),
-  ));
+  runApp(MyApp());
 }
 
-class FistApp extends StatefulWidget {
-  FirstRoute createState() => new FirstRoute();
-}
-
-class FirstRoute extends State<FistApp> {
-  bool _cb = false;
-  bool _sw = false;
-  void _cbChanged(bool value) => setState(() => _cb = value);
-  void _swChanged(bool value) => setState(() => _sw = value);
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('First Route'),
-      ),
-      body: Column(
-        children: [
-          Text('Test'),
-          TextField(
-            obscureText: true,
-            decoration:
-                InputDecoration(border: OutlineInputBorder(), labelText: 'ID'),
+    return Container(
+      decoration: BoxDecoration(color: Colors.white),
+      child: Center(
+        child: Text(
+          'Hello World',
+          textDirection: TextDirection.ltr,
+          style: TextStyle(
+            fontSize: 32,
+            color: Colors.black87,
           ),
-          TextField(
-            obscureText: true,
-            decoration:
-                InputDecoration(border: OutlineInputBorder(), labelText: 'PWD'),
-          ),
-          Checkbox(
-            value: _cb,
-            onChanged: _cbChanged,
-          ),
-          Switch(value: _sw, onChanged: _swChanged),
-          RaisedButton(
-            child: Text('Open route'),
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => SecondRoute()),
-              );
-            },
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class SecondRoute extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Second Route"),
-      ),
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: Text('Go back!'),
         ),
       ),
     );
